ab98bff4c030c90c3dacf53f4fd80a7f1ae8cfe8
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-08 13:57:23 +0530

    NPM-7 - Alternative way of removing packages

---
+++ 23-alternative-way-to-uninstall-packages.js
@@ -0,0 +1,12 @@
+/*
+
+the 2nd method of uninstalling packages is as follows
+=====================================================
+(this approach is just like we have adding and removing packages in Flutter pubcpec.ymal)
+
+(*) delete 'package-lock.json' file
+(*) delete 'node_modules' folder
+(*) remove dependency from 'package.json' file
+(*) and then run the command 'npm install' or 'npm i' again
+
+*/
