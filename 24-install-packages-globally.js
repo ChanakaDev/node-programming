46eee9af40b1c4335c3333c31f83f9f6441453cc
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-08 14:20:08 +0530

    NPM-8 - Install packages globally

---
+++ 24-install-packages-globally.js
@@ -0,0 +1,16 @@
+/*
+
+how to install packages(modules) globally
+=========================================
+
+(*) you can run these commands even in your Terminal(macbook) or Cmd(windows).
+(*) example: 'sudo npm install -g nodemon' (I ran this command in my macbook)
+
+(*) you can use globally installed packages in any project
+    examples: React, nodemon
+
+important: you no need to install React globally, because 'npx' is there
+
+(*) for example if you run 'npx create-react-app my-app', we don't need to install React globally
+
+*/
