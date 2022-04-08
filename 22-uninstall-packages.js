ec7ce4cd942cc9048d62ff53a9b1f8247d8cdb31
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-08 13:52:25 +0530

    NPM-6 -  Uninstall packages

---
+++ 22-uninstall-packages.js
@@ -0,0 +1,10 @@
+/*
+
+(*) in order to uninstall the packages, all you have to do is run following command
+'npm uninstall <packageName>' in the Terminal.
+
+(*) for example let's try 'npm uninstall bootstrap'
+
+output: // removed 2 packages in 633ms
+
+*/
