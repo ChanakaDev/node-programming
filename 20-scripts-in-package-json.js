e8bb2b012cca06031311abef97d394eb65788aaa
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-08 13:35:34 +0530

    NPM-4 - How to work npm start

---
+++ 20-scripts-in-package-json.js
@@ -0,0 +1,13 @@
+/*
+
+(*) untill now, we have run js files manually. but you can automate that inside 'package.json' file.
+(*) all you have to do is to add
+
+"scripts": {
+    "start" : "node app.js"
+},
+
+(*) then all you have to do is to run the command 'npm start'
+(*) then according to the above script it will run the file called 'app.js'
+
+*/
