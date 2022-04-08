a5305c1d2fba5a460c98dfc270633b1efaabb213
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-08 13:48:04 +0530

    NPM-5 - How does the noodemon works

---
+++ 21-dev-in-scripts.js
@@ -0,0 +1,20 @@
+/*
+
+(*) if nodemon need to whatch your code before running
+(*) you can add 
+
+  "scripts": {
+    "start": "node app.js",
+    "dev": "nodemon app.js" // this line
+},
+
+to the scripts. 
+
+(*) then, when you run the command 'npm start', nodemon view the code inside 'app.js'
+
+however this didn't worked in my dev environment :(
+
+(*) actually what does the nodemon do is, it runs the code automatically, when we do any change in 'app.js'
+(*) you can see this functionality most of the times in 'ReactJS'
+
+*/
