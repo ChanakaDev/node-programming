df7797af9581cbd219cff4b942e5aeeec0da5e9b
ChanakaDev <mr.mbcnb@gmail.com>
2022-04-10 18:34:38 +0530

    NPM-12 - Http request basics

---
+++ 33-check-http-request-details.js
@@ -0,0 +1,7 @@
+/*
+
+if we inspect a web page and look at the 'Network' tab 'All', there you can see all the 
+infomation related to a particular http request (you can see both request and response
+messages there)
+
+*/
